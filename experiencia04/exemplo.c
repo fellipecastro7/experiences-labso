11d315316b35e399b0bed21fe846a11d8c9c803a
fellipecastro7 <fellipemachado77@gmail.com>
2022-12-28 21:49:24 -0300

    Create exemplo.c

---
+++ experiencia04/exemplo.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define MAX_COUNT 200
+
+void ChildProcess(void);
+void ParentProcess(void);
+
+void main(void) {
+    pid_t pid;
+    
+    pid = fork();
+
+    printf("PID: %d\n", pid);
+    
+    if (pid == 0) {
+        ChildProcess();
+    }
+
+    else {
+        ParentProcess();
+    }
+
+    exit(0);
+}
+
+void ChildProcess(void) {
+    int i;
+    
+    for (i = 1; i <= MAX_COUNT; i++) {
+        printf(" Processo FILHO, valor = %d\n", i);
+    }
+    
+        printf(" *** Processo FILHO finalizado ***\n");
+}
+
+void ParentProcess(void) {
+    int i;
+    
+    for (i = 1; i <= MAX_COUNT; i++) {
+        printf("Processo PAI, value = %d\n", i);
+    }
+    
+    printf("*** Processo PAI finalizado ***\n");
+    exit(0);
+}
